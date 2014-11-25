1a097b0b34951483bb7810156689a1be4086e078
Iaroslav Sergieiev <noomorph@gmail.com>
2014-11-25 15:14:23 +0200

    data/polls.js

--- data/polls.js
+++ data/polls.js
@@ -2,26 +2,29 @@ module.exports = [
     {
         id: 1,
         question: 'Are you familiar with this syntax?',
-        answers: [ 'Yes', 'No' ]
+        answers: [
+            { key: 'Yes', color: '#0f0' },
+            { key: 'No', color: '#000' }
+        ]
     },
     {
         id: 2,
         question: 'Which browser implements most of ES6?',
-        answers: {
-            A: 'Chrome',
-            B: 'Firefox',
-            C: 'IE',
-            D: 'Safari',
-            E: 'Opera'
-        }
+        answers: [
+            { key: 'A', name: 'Chrome' },
+            { key: 'B', name: 'Firefox' },
+            { key: 'C', name: 'IE' },
+            { key: 'D', name: 'Safari' },
+            { key: 'E', name: 'Opera' }
+        ]
     },
     {
         id: 3,
         question: 'What will happen in this case?',
-        answers: {
-            A: 'Throws exception',
-            B: 'Ignores assignemnt',
-            C: 'Assigns the value'
-        }
+        answers: [
+            { key: 'A', name: 'Throws exception', color: '#f00', },
+            { key: 'B', name: 'Ignores assignemnt', color: '#fa0' },
+            { key: 'C', name: 'Assigns the value', color: '#4f4' }
+        ]
     }
 ];

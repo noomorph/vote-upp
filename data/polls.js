e3c186098d1817b7c8f16d968409dc85183ce32e
Iaroslav Sergieiev <noomorph@gmail.com>
2014-12-09 09:30:18 +0200

    poll updated

--- data/polls.js
+++ data/polls.js
@@ -5,23 +5,11 @@ var Poll = require('../lib/poll');
 var polls = [
     new Poll({
         id: 0,
-        question: 'Нравится ли идея с завтраками?',
+        question: 'Как вам завтрак сегодня?',
         revotable: true,
         answers: [
-            { key: 'A', name: 'Да' },
-            { key: 'B', name: 'Нет' }
-        ]
-    }),
-    new Poll({
-        id: 1,
-        question: 'Лучший завтрак был в..?',
-        answers: [
-            { key: 'A', name: 'Понедельник' },
-            { key: 'B', name: 'Вторник' },
-            { key: 'C', name: 'Еще не был' }
-            //{ key: 'C', name: 'Среду' },
-            //{ key: 'D', name: 'Четверг' },
-            //{ key: 'E', name: 'Пятницу' }
+            { key: 'A', name: 'Нравится' },
+            { key: 'B', name: 'Не очень' }
         ]
     })
 ];
